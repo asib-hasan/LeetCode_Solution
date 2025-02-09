68be0305d6bbd8ef04a76b0cdf949691690f9b1e
Asib Hasan <asib.uucse@gmail.com>
2025-02-09 10:41:03 +0600

    Time: 0 ms (100%), Space: 13.7 MB (5.32%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        if(right == 1 && nums[0] == target) return 0;
+        else if(right == 1 && nums[0] < target) return 1;
+        else if(right == 1 && nums[0] > target) return 0;
+        int mid;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            if(mid >= nums.size()) return nums.size();
+            else if(mid < 0) return 0;
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if(nums[mid] > target) return mid;
+        else return mid + 1;
+    }
+};
