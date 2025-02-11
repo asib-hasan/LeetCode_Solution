c63bc654c78702515ceb4600bd76367dd69aa959
Asib Hasan <asib.uucse@gmail.com>
2025-02-11 23:52:05 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n == 0 || n == 1) return 0;
+        vector<int> isPrime(n + 1, true);
+        isPrime[0] = false, isPrime[1] = false;
+        int cnt = 0;
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for(int i=0;i<isPrime.size();i++){
+            if(isPrime[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
